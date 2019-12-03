e9e0e1bf393a34f1cef75a120ffd401dab882b68
Luis Miguel <inf.lrodriguez@gmail.com>
2019-12-02 21:30:52 -0400

    test a services

---
+++ test/services.movies.test.js
@@ -0,0 +1,28 @@
+const assert = require("assert")
+const proxyquire = require("proxyquire")
+
+const { MongoLibMock, getAllStub } = require("../utils/mocks/mongoLib")
+
+const { moviesMock } = require("../utils/mocks/movies")
+
+
+describe('services - movies ', function name() {
+    const MoviesService = proxyquire("../services/movies", {
+        "..lib/mongo": MongoLibMock
+    })
+
+    const moviesService = new MoviesService()
+
+    describe("when getMovies mehtod is called", async function () {
+        it("should call the getall MongoLib method", async function () {
+            await moviesService.getMovies()
+            assert.strictEqual(getAllStub, true)
+        })
+        it("should return an array of movies", async function () {
+            const result = await moviesService.getMovies()
+            const expected = moviesMock
+            assert.deepEqual(result, expected)
+
+        })
+    })
+})
